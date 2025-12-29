6432e89cc54ab8cea2dccf61e6a4c4d3dccdda2d
liu <lchenglong789@gmail.com>
2025-12-29 17:44:20 +0800

    Update vim_model.py

--- dcase_task/nnet/vim/vim_model.py
+++ dcase_task/nnet/vim/vim_model.py
@@ -7,7 +7,7 @@ class VimMamba2(torch.nn.Module):
     def __init__(self, mamba2_path, *args, vim_dropout=0.0, **kwargs, ) -> None:
         super().__init__(*args, **kwargs)
         self.mamba2 = FrameVimModel(mamba2_dropout=vim_dropout)
-        # self.load_mamba2(mamba2_path)
+        self.load_mamba2(mamba2_path)
         self.fake_length = torch.tensor([1001])
         self.cls_embed = None
 
@@ -27,18 +27,65 @@ class VimMamba2(torch.nn.Module):
         return mamba2_x
 
 
-def main():
-    # 创建模型
-    model = VimMamba2(mamba2_path=None, vim_dropout=0.0)
-    model.eval()
-    # 输入：8 batch，64 dim，1001 长度
-    x = torch.randn(8, 64, 1001)
-    # 前向
-    with torch.no_grad():
-        out = model(x)
+    def load_vim(self, path=None):
+        if path is None:
+            pre_path = "../nnet/ckpts/last.ckpt"
+            assert os.path.exists(pre_path), "Please make sure you have a default path to load vim. Please change this path to the vim_as2M.ckpt that you downloaded."
+            path = pre_path    # Change path to the vim_as2M.ckpt the downloaded checkpoint from the home page.
+        state_dict = torch.load(path, map_location="cpu")["state_dict"]
+        vim_state_dict = {}
+        for k, v in state_dict.items():
+            if "model.teacher.encoder." in k:
+                print("model.teacher.encoder")
+                if "encoder.norm." in k:
+                    new_k = k.replace("model.teacher.encoder.norm", "norm_frame")
+                elif "cls_token" in k:
+                    continue
+                else:
+                    new_k = k.replace("model.teacher.encoder.", "")
+                vim_state_dict[new_k] = v
+            # C2F
+            if "encoder.encoder.frame_encoder." in k:
+                print("encoder.encoder.frame_encoder")
+                new_k = k.replace("encoder.encoder.frame_encoder.", "")
+                vim_state_dict[new_k] = v
+                continue
+            if "encoder.encoder.teacher_module." in k:
+                continue
+            # Frame
+            if "encoder.encoder." in k:  # This is
+                print("encoder.encoder")
+                new_k = k.replace("encoder.encoder.", "")
+                vim_state_dict[new_k] = v
 
-    print("输入 shape:", x.shape)
-    print("输出 shape:", out.shape)
+        self.vim.load_state_dict(vim_state_dict, strict=True)
+        for n, param in self.vim.named_parameters():
+            param.requires_grad = False
+        # state_dict = torch.load(path, map_location="cpu")["sed_teacher"]
+        # vim_state_dict = {}
+        # for k, v in state_dict.items():
+        #     if ("total_ops" in k) or ("total_params" in k):
+        #         continue
+        #     if "vim_frame.vim." in k:
+        #         k = k.replace("vim_frame.vim.", "")
+        #         vim_state_dict[k] = v
+        # self.vim.load_state_dict(vim_state_dict, strict=True)
+        # for n, param in vim.named_parameters():
+        #     param.requires_grad = False
 
-if __name__ == "__main__":
-    main()
+
+# def main():
+#     # 创建模型
+#     model = VimMamba2(mamba2_path=None, vim_dropout=0.0)
+#     model.eval()
+#     # 输入：8 batch，64 dim，1001 长度
+#     x = torch.randn(8, 64, 1001)
+#     # 前向
+#     with torch.no_grad():
+#         out = model(x)
+# 
+#     print("输入 shape:", x.shape)
+#     print("输出 shape:", out.shape)
+# 
+# if __name__ == "__main__":
+#     main()
